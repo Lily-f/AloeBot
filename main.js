d40841cc2e6c1e3e43ae8334116ac25e236f1ae3
Lily Fahey <lily.fahey.99@gmail.com>
2020-10-27 13:57:47 +1300

    Adds echo

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 import { Client } from 'discord.js';
 import dotenv from 'dotenv';
 
+// Add .env variables to the process. Used to obtain tokens
 dotenv.config();
-const client = new Client();
+const aloeBot = new Client();
+const aloePrefix = '^';
 
-client.once('ready', () => {
-  console.log('Aloe is online!');
-  console.log('test');
+// Responce to successful login once
+aloeBot.once('ready', () => {
+  console.log('Aloe online!');
+  console.log(aloeBot.user.id);
 });
 
-client.login(process.env.DISCORD_TOKEN);
+// Respond to messages
+aloeBot.on('message', (message) => {
+  // Only respond with the right prefix
+  if (!message.content.startsWith(aloePrefix)) return;
+  message.channel.send(message.content.substring(1, message.content.length));
+});
+
+// Login the bot to discord. MUST BE LAST LINE
+aloeBot.login(process.env.DISCORD_TOKEN);
