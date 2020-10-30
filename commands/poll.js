3b6413568e388f974fd90a676f1d7a49627e9c9b
Lily Fahey <lily.fahey.99@gmail.com>
2020-10-31 01:37:08 +1300

    Adds poll tmeout check and valid emoji regex

--- commands/poll.js
+++ commands/poll.js
@@ -17,17 +17,26 @@ const poll = {
     // Find topic, determined by two quotes in the mesasge
     const matches = message.content.match(/"/g);
     const quotesNumber = matches ? matches.length : 0;
-    if (quotesNumber < 2) message.reply('Poll topic command requires at least 2 double quote marks!');
-
-    // Get topic from message and all args beyond topic
+    if (quotesNumber < 2) {
+      message.reply('Poll topic command requires at least 2 double quote marks!');
+      return;
+    }
     const topic = message.content.substring(message.content.indexOf('"') + 1, message.content.lastIndexOf('"'));
+
+    // Get and check arguments for the command
     const messageAfterTopic = message.content.substring(message.content.lastIndexOf('"') + 1);
     const args = messageAfterTopic.trim().split(/ +/);
 
     const timeout = parseInt(args[0], 10);
+    if (Number.isNaN(timeout) || timeout <= 0) {
+      message.reply('Timeout needs to be a number greater than 0!');
+      return;
+    }
     const forceEndReact = args[1];
+    const unicodeEmogiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/;
+    console.log(unicodeEmogiRegex.test(forceEndReact));
     const reactOptions = args.slice(2, args.length);
-    message.channel.send(`making a poll! topic:'${topic}', timeout:'${timeout}', endReact:'${forceEndReact}, reactOptions:'${reactOptions}'`);
+    message.channel.send(`making a poll! topic:'${topic}', timeout:'${timeout}', endReact:'${forceEndReact}', reactOptions:'${reactOptions}'`);
   },
 };
 export default poll;
