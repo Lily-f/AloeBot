8838720a7ca6224f470a0b24d0e77097d0833aaf
Lily Fahey <lily.fahey.99@gmail.com>
2020-11-05 01:16:07 +1300

    Changes shuffle to be simpler but still Fisher-Yates

--- util/shuffle.js
+++ util/shuffle.js
@@ -1,25 +1,17 @@
 /**
+ * Fisher-Yates Shuffle on an array. Returns a shuffled copy of a given array
+ * Doesn't mutate the given array
+ *
  * @param {any[]} givenArray array to shuffle
  * @returns {any[]} shuffled array
  */
 function shuffle(givenArray) {
   const array = givenArray;
-  let currentIndex = array.length;
-  let temporaryValue;
-  let randomIndex;
-
-  // While there remain elements to shuffle...
-  while (currentIndex !== 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+  let count = array.length;
+  while (count) {
+    array.push(array.splice(Math.floor(Math.random() * count), 1)[0]);
+    count -= 1;
   }
-
   return array;
 }
 
