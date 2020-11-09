1b37a2e8b22a3a76910d5266f5bdc51a3f87fd52
Lily Fahey <lily.fahey.99@gmail.com>
2020-11-09 23:12:26 +1300

    Adds game type check for playing a card

--- commands/play-card.js
+++ commands/play-card.js
@@ -1,4 +1,7 @@
 const Message = require('discord.js');
+const HeartsGame = require('../game-types/hearts-game.js');
+const CardGame = require('../util/card-game.js');
+const UpDownRiverGame = require('../game-types/up-down-river-game.js');
 
 const playCard = {
   name: 'play-card',
@@ -12,8 +15,25 @@ const playCard = {
    * Play card command. Handles when a user plays a card for a game
    *
    * @param {Message} message user invocation message
+   * @param {string[]} args user given arguments for the comand
    */
-  execute(message) {
+  execute(message, args) {
+    // Get game user is in
+    const game = message.client.games.get(message.author.id);
+    if (!game) {
+      message.reply('You\'re not in a card game!');
+      return;
+    }
+
+    // TODO: get the user and card being played
+
+    // TODO: Check the user has the card being played
+    // TODO: Play the card based on the gametype the user is playing
+    if (game instanceof HeartsGame) {
+      message.reply('You\'re playing hearts!');
+    } else if (game instanceof UpDownRiverGame) {
+      message.reply('You\'re playing Up and Down the River');
+    }
   },
 };
 module.exports = playCard;
---
+++ game-types/up-down-river-game.js
@@ -0,0 +1,6 @@
+const Game = require('../util/card-game.js');
+
+class UpDownRiverGame extends Game {
+
+}
+module.exports = UpDownRiverGame;
