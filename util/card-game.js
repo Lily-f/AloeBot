0bdad4fc144be9e1e5bdbefc6c615692aec23a98
Lily Fahey <lily.fahey.99@gmail.com>
2020-11-09 22:46:17 +1300

    Adds stub for playing card in card game

--- util/card-game.js
+++ util/card-game.js
@@ -1,3 +1,4 @@
+const { Message } = require('discord.js');
 const Player = require('./card-player.js');
 const { Card, suits, values } = require('./card.js');
 const shuffle = require('./shuffle');
@@ -29,6 +30,20 @@ class CardGame {
         return suits.indexOf(a.suit) - suits.indexOf(b.suit);
       });
     });
+  }
+
+  /**
+   * Play a given card from a given player if tey are the active player
+   *
+   * @param {object} config configuration for player and card being played
+   * @param {Message} config.message message calling this function
+   */
+  playCard(config) {
+    if (config.message.author.id !== this.activePlayer) {
+      console.log(`${config.message.author.id} is not the active player`);
+      return;
+    }
+    console.log(`Active player ${config.message.author.id} is playing a card`);
   }
 }
 module.exports = CardGame;
