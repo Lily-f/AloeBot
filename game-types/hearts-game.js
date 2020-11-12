0c81efa96a5bd767b8234c2e927008355e5a61ed
Lily Fahey <lily.fahey.99@gmail.com>
2020-11-13 00:09:53 +1300

    Changes hearts play card to be modular

--- game-types/hearts-game.js
+++ game-types/hearts-game.js
@@ -1,4 +1,4 @@
-const { Message } = require('discord.js');
+const { Message, Channel } = require('discord.js');
 const Game = require('./card-game.js');
 const { Card, values } = require('../util/card.js');
 const Player = require('../util/card-player.js');
@@ -37,16 +37,13 @@ class HeartsGame extends Game {
       config.message.reply(`You didn't follow suit! Play ${this.currentTrick[0].card.suit}`);
       return false;
     }
+
     // Add played card to current trick, set next player to active
-    config.message.reply(`You played \`${config.card.toString()}\``);
-    this.currentTrick.push({ card: config.card, player: config.player });
-    let nextPlayerIndex = this.players.findIndex((p) => p.userId === this.activePlayerId) + 1;
-    if (nextPlayerIndex === this.players.length) nextPlayerIndex = 0;
-    this.activePlayerId = this.players[nextPlayerIndex].userId;
-    this.activePlayerName = this.players[nextPlayerIndex].username;
+    this.rotatePlay(config);
 
     // Handle the end of the trick
     if (this.currentTrick.length === this.players.length) {
+      // Calculate winning play in trick
       let winningPlay = this.currentTrick[0];
       this.currentTrick.forEach((play) => {
         if (play.card.suit === winningPlay.card.suit
@@ -55,37 +52,77 @@ class HeartsGame extends Game {
         }
       });
 
+      // Set winner as next active player
       this.activePlayerId = winningPlay.player.userId;
       this.activePlayerName = winningPlay.player.username;
 
-      const heartsThisTrick = [];
-      this.currentTrick.forEach((play) => {
-        if (play.card.suit === 'HEARTS') {
-          winningPlay.player.wonCards.push(play.card);
-          heartsThisTrick.push(play.card);
-        }
-      });
-      if (heartsThisTrick.length) {
-        config.message.channel.send(`${winningPlay.player.username} has won ${heartsThisTrick}`);
-      } else {
-        config.message.channel.send(`${winningPlay.player.username} won the trick. No hearts`);
-      }
+      // Find hearts in trick and display who won them
+      this.displayWonHearts(config.message.channel, winningPlay);
       this.currentTrick = [];
 
       // Handle if that was the last card in the game
-      if (config.player.hand.length === 1) {
-        let scoresMessage = '';
-        this.players.forEach((player) => {
-          let score = 0;
-          player.wonCards.forEach((card) => {
-            score += values.indexOf(card.value) + 2;
-          });
-          scoresMessage += `${player.username}: ${score}\n`;
-        });
-        config.message.channel.send(`GAME OVER. Scores are:\n${scoresMessage}`);
-      }
+      this.handleEnding(config);
     }
     return true;
+  }
+
+  /**
+   * Add card to current trick and get next player
+   *
+   * @param {object} config configuration for player and card being played
+   * @param {Message} config.message message calling this function
+   * @param {Card} config.card Card to play
+   * @param {Player} config.player player playing the card
+   */
+  rotatePlay(config) {
+    config.message.reply(`${config.player.username} played \`${config.card.toString()}\``);
+    this.currentTrick.push({ card: config.card, player: config.player });
+    let nextPlayerIndex = this.players.findIndex((p) => p.userId === this.activePlayerId) + 1;
+    if (nextPlayerIndex === this.players.length) nextPlayerIndex = 0;
+    this.activePlayerId = this.players[nextPlayerIndex].userId;
+    this.activePlayerName = this.players[nextPlayerIndex].username;
+  }
+
+  /**
+   * Handle ending of the game, if appropriate
+   *
+   * @param {object} config configuration for player and card being played
+   * @param {Message} config.message message calling this function
+   * @param {Player} config.player player playing the card
+   */
+  handleEnding(config) {
+    if (config.player.hand.length === 1) {
+      let scoresMessage = '';
+      this.players.forEach((player) => {
+        let score = 0;
+        player.wonCards.forEach((card) => {
+          score += values.indexOf(card.value) + 2;
+        });
+        scoresMessage += `${player.username}: ${score}\n`;
+      });
+      config.message.channel.send(`GAME OVER. Scores are:\n${scoresMessage}`);
+    }
+  }
+
+  /**
+   * Calculate and display hearts won in the current trick
+   *
+   * @param {Channel} channel channel to display results
+   * @param {object} winningPlay card and player of the tricks winning play
+   */
+  displayWonHearts(channel, winningPlay) {
+    const heartsThisTrick = [];
+    this.currentTrick.forEach((play) => {
+      if (play.card.suit === 'HEARTS') {
+        winningPlay.player.wonCards.push(play.card);
+        heartsThisTrick.push(play.card);
+      }
+    });
+    if (heartsThisTrick.length) {
+      channel.send(`${winningPlay.player.username} has won ${heartsThisTrick}`);
+    } else {
+      channel.send(`${winningPlay.player.username} won the trick. No hearts gained`);
+    }
   }
 }
 module.exports = HeartsGame;
