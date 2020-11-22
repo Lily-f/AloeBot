17064c38967709fd928cafa3fb014888362f21bb
Lily Fahey <lily.fahey.99@gmail.com>
2020-11-23 00:18:08 +1300

    Changes hearts to use generic game creation

--- commands/hearts.js
+++ commands/hearts.js
@@ -1,6 +1,6 @@
 const { Message, MessageEmbed } = require('discord.js');
 const HeartsGame = require('../game-types/hearts-game.js');
-const Player = require('../util/card-player.js');
+const { checkValidPlayers, readPlayers } = require('../util/game-creation.js');
 const { generateDeck } = require('../util/card.js');
 const config = require('../config.js');
 
@@ -18,36 +18,15 @@ const hearts = {
    * @param {Message} message  user invocation message
    */
   execute(message) {
-    const players = [];
-    players.push(message.author);
-    message.mentions.users.forEach((user) => {
-      if (!players.includes(user) && !user.bot) players.push(user);
+    // Read discord users (players) from command
+    const players = readPlayers({
+      message, minPlayers: 2, maxPlayers: 5, gamename: 'Hearts',
     });
+    if (!players) return;
 
-    // Check there are between 3 and 5 (inclusive) players for the game
-    if (players.length < 2 || players.length > 5) {
-      message.reply('Hearts needs 3-5 players! "@" 1-4 other players for a game (no bots)');
-      return;
-    }
-
-    // Check none of the players are already in a game
-    const playersAlreadyInGame = [];
-    players.forEach((potentialPlayer) => {
-      if (message.client.usersInGames.includes(potentialPlayer.id)) {
-        playersAlreadyInGame.push(potentialPlayer.username);
-      }
-    });
-    if (playersAlreadyInGame.length) {
-      message.reply(`Users: ${playersAlreadyInGame.join(', ')} are already in a game!`);
-      return;
-    }
-
-    // Create player instances for the game
-    const gamePlayers = new Map();
-    players.forEach((player) => {
-      gamePlayers.set(player.id, new Player({ username: player.username, userId: player.id }));
-      message.client.usersInGames.push(player.id);
-    });
+    // Create playr instances for users
+    const gamePlayers = checkValidPlayers({ message, players });
+    if (!gamePlayers) return;
 
     // Create the deck for the game (remove cards based on player count)
     let deck = [];
@@ -72,14 +51,7 @@ const hearts = {
     message.channel.send(response);
 
     // Tell user their cards
-    const playerCards = new MessageEmbed()
-      .setColor(config.color)
-      .setTitle('You\'ve started a Hearts game!');
-    players.forEach((player) => {
-      const playerHand = [];
-      gamePlayers.get(player.id).hand.forEach((card) => { playerHand.push(card.toString()); });
-      player.send(playerCards.setDescription(`Your cards are: \n\`${playerHand.join(', ')}\``));
-    });
+    game.displayCards(players);
   },
 };
 module.exports = hearts;
